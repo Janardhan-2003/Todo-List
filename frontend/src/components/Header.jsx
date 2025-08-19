67ebfeccb9e5ac01a58abe242f020c2fcb9d0cca
Janardhan-2003 <kokatam71714@gmail.com>
2025-08-19 12:19:49 +0530

    small changes in frontend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,38 +1,46 @@
-import React from 'react'
-import { useNavigate, useLocation } from 'react-router-dom';
-import { signOut } from 'firebase/auth';
-import { auth } from '../auth/FirebaseConfig/Firebase';
-import Cookies from 'js-cookie';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../auth/FirebaseConfig/Firebase";
+import Cookies from "js-cookie";
 
 const Header = () => {
+  const navigate = useNavigate();
+  
 
-   const navigate=useNavigate()
-  const location=useLocation()
+ 
+const handleLogout = async () => {
+  const auth = getAuth();
+  try {
+    await signOut(auth);
+    
+    // remove cookies
+    Cookies.remove("authToken");
+    Cookies.remove("userId");
+    Cookies.remove("UserDetails");
 
-  const handleLogout = () => {
-    signOut(auth)
-    .then(()=>{
-      Cookies.remove('authToken');
-      window.location.href='/login';
-    })
-    .catch(error=>{
-      console.error('Logout Error:', error)
-    })
-  };
+    navigate("/login");
+  } catch (e) {
+    console.error("Error signing out:", e);
+  }
+};
   return (
-    <div className='flex justify-between items-center bg-white p-4 shadow-md w-full px-10 sticky left-0'>
-        <div>
-            <h1 className='text-3xl font-bold font-serif text-sky-500 '>Todo</h1>
-        </div>
-        <div className='flex gap-4'>
-            <p>Home</p>
-            <p>about</p>
-            <button className='bg-red-500 text-white w-20 h-8 rounded-2xl font-medium font-serif text-shadow-slate-950 shadow-md hover:bg-red-600 cursor-pointer border border-slate-900'
-            onClick={handleLogout}
-            >Logout</button>
-        </div>
+    <div className="flex justify-between items-center bg-white p-4 shadow-md w-full px-10 sticky left-0">
+      <div>
+        <h1 className="text-3xl font-bold font-serif text-sky-500 ">Todo</h1>
+      </div>
+      <div className="flex gap-4">
+        <p>Home</p>
+        <p>about</p>
+        <button
+          className="bg-red-500 text-white w-20 h-8 rounded-2xl font-medium font-serif text-shadow-slate-950 shadow-md hover:bg-red-600 cursor-pointer border border-slate-900"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
